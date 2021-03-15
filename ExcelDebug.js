3fc1feedcf10eb1f2a4d941e044be5a589b56a6b
John <gbcritic@hotmail.com>
2021-03-15 11:52:24 +0000

    New setup (#1)
    * Added new stuff
    
    * Debugging sheet and header names
    
    * updated bug script
    
    * updated bug script. again
    
    Co-authored-by: = <=>

---
+++ ExcelDebug.js
@@ -0,0 +1,90 @@
+const filePath = process.env.npm_config_filepath;
+
+if (filePath === undefined || "") {
+    throw Error("filepath to spreadsheet is not provided");
+}
+
+const fs = require('fs');
+const reader = require('xlsx');
+
+const jsonFileName = "data.json";
+const file = reader.readFile("./generic_multiple.xlsx");
+
+let data = [];
+
+const sheets = file.SheetNames;
+
+let debugJSON = [];
+
+function createHeaders(headers) {
+    let headerObjects = [];
+
+    for (let index = 0; index < headers.length; index++) {
+        let tempHeader = headers[index];
+        
+        let headerObj = {
+            headerOriginalText: tempHeader.trim(),
+            headerJSONLabel: castToPascalCase(tempHeader),
+            headerIndex: index,
+            ignoreFlag: false
+        }
+
+        headerObjects.push(headerObj);
+    }
+
+    return headerObjects;
+}
+
+function castToPascalCase(val) {
+    return val.trim().toLowerCase().replace(/ /g, "_");
+}
+
+for(let i = 0; i < sheets.length; i++) {
+    const sheetname = file.SheetNames[i];
+
+    const newObj = {};
+    newObj["SheetName"] = sheetname;
+    const temp = reader.utils.sheet_to_json(file.Sheets[file.SheetNames[i]], {header: 1});
+
+    // IGNORE FIRST ROW
+    newObj["NodesCount"] = temp.length - 1;
+
+    const worksheet = file.Sheets[file.SheetNames[i]];
+
+    debugJSON.push(worksheet);
+
+    newObj.data = [];
+
+    let headerObjs = createHeaders(temp[0]);
+
+    for (let rowIndex = 0; rowIndex < temp.length; rowIndex++) {
+        // IGNORE FIRST ROW HEADER
+        if (rowIndex === 0) {
+            continue;
+        }
+        
+        let row = temp[rowIndex];
+
+        let record = {};
+
+        for (let columnIndex = 0; columnIndex < row.length; columnIndex++) {
+            const column = row[columnIndex];
+            const columnTitle = headerObjs[columnIndex].headerJSONLabel;
+            
+            record[columnTitle] = column;
+        }
+
+        newObj.data.push(record);
+
+    }
+
+    data.push(newObj);
+}
+
+let jsonStr = JSON.stringify(data);
+
+// WRITE JSON DATA TO FILE
+fs.writeFile(jsonFileName, jsonStr, function (err) {
+    if (err) return console.log(err);
+    console.log('Completed Excel to JSON > ' + jsonFileName);
+});
